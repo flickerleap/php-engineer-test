03f1b89870ffb11fdc24147c030ca4baacdd5e1c
Leo Gopal <leo@leogopal.com>
2017-08-28 16:03:41 +0200

    Diamond first iteration.

--- includes/Diamond.php
+++ includes/Diamond.php
@@ -1,6 +1,74 @@
 <?php
 /*
- * The Diamond Class
+ * Diamond Class
  */
 
 namespace FlickerLeap;
+
+/**
+ * Class Daimond
+ *
+ * @package FlickerLeap
+ */
+class Diamond extends Shape
+{
+    /**
+     *
+     * @param int $length
+     */
+    public function __construct($length = 5)
+    {
+        $this->name       = 'Diamond';
+        $this->sides      = 4;
+        $this->sideLength = $length;
+        $this->pixel      = "*";
+    }
+
+    /**
+     * Displays name of the shape.
+     */
+    public function displayName()
+    {
+        echo $this->name;
+
+    }
+
+    /**
+     * Draws the diamond.
+     */
+    public function draw()
+    {
+        $space = $this->sideLength;
+
+        for ($i = 1; $i <= $this->sideLength; $i++) {
+            for ($j = 1; $j <= $space; $j++) {
+                echo $this->padding(2);
+            }
+
+            $space--;
+
+            for ($j = 1; $j <= (2 * $i) - 1; $j++) {
+                echo $this->pixel;
+            }
+
+            $this->newLine();
+        }
+
+        $space = 2;
+
+        for ($i = 1; $i <= $this->sideLength; $i++) {
+
+            for ($j = 1; $j <= $space; $j++) {
+                echo $this->padding(2);
+            }
+
+            $space++;
+
+            for ($j = 1; $j <= 2 * ($this->sideLength - $i) - 1; $j++) {
+                echo $this->pixel;
+            }
+
+            $this->newLine();
+        }
+    }
+}
