76d7c34c7094853ef3e4eb3557e389fef8ead6ed
KennethSylar <verbalsylar@gamil.com>
2021-03-15 21:30:03 +0200

    step 2

--- includes/Diamond.php
+++ includes/Diamond.php
@@ -4,3 +4,47 @@
  */
 
 namespace FlickerLeap;
+
+/**
+ *
+ */
+class Diamond extends Shape
+{
+    /**
+     *
+     * @param int $length
+     */
+    public function __construct($length) {
+        $this->name = 'Diamond';
+        $this->sides = 1 - $length;
+        $this->sideLength = $length;
+        $this->pixel = "*";
+    }
+
+    /**
+     *
+     */
+    public function displayName()
+    {
+        echo $this->name;
+
+    }
+
+    /**
+     * Draws the Diamond.
+     */
+     public function draw()
+     {
+       for ($i= $this->sides ; $i <= ($this->sideLength - 1); $i++) {
+         for ($j=$this->sides; $j <= $this->sideLength -1 ; $j++) {
+            if (($j + $this->sideLength -1 == -$i) || ($j + $this->sideLength - 1 == $i) || ($j - $this->sideLength + 1 == -$i) || ($j - $this->sideLength + 1 == $i)) {
+               echo $this->pixel . $this->padding(2);
+            }
+            else {
+              echo $this->padding(2);
+            }
+         }
+         $this->newLine();
+       }
+     }
+ }
