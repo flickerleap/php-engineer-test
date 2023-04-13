e2dee29caa6614dd3105504b7341307f9c144120
LuxMagesh <luxmotsumi@gmail.com>
2023-04-13 14:53:11 +0200

    Output The Square
    Output Output The Square

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 use FlickerLeap\Diamond;
 use FlickerLeap\Rectangle;
+use FlickerLeap\Square;
 
 ?>
 <!DOCTYPE html>
@@ -31,6 +32,8 @@ use FlickerLeap\Rectangle;
 
         <?php
             // implement the square class here
+            $testObject = new Square();
+            $testObject->draw();
         ?>
 
         <h2>Output a diamond</h2>
